0b0f177476195288c9fac8fc9a4f8e400f67fa90
gt22 <igorengel@mail.ru>
2019-08-10 21:34:09 +0300

    VimPlug

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,6 +1,11 @@
 CURDIR="$(cd "$(dirname "$0")"; pwd -P)"
+if [ ! -f ~/.vim/autoload/plug.vim ]; then
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 mkdir -p ~/Library/texmf/tex
 ln -s $CURDIR/preamble ~/Library/texmf/tex/latex
 mkdir -p ~/.vim/
 ln -s $CURDIR/snips/main ~/.vim/UltiSnips
 ln -s $CURDIR vimrc ~/.vimrc
+vim +PlugInstall +qall
